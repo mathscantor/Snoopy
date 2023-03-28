6a5ad4c1f91e0e7f0ed50395f25db6d8ff711870
mathscantor <geraldlim619@gmail.com>
2023-03-28 17:05:10 +0800

    Added feature to saved captured packets as pcapng file

--- snoopy.py
+++ snoopy.py
@@ -2,6 +2,7 @@ import socket
 from utils.packet import Packet
 import gc
 import argparse
+from utils.pcapng_saver import PcapngSaver
 from utils.layer_parsers.link import *
 from utils.layer_parsers.network import *
 from utils.layer_parsers.transport import *
@@ -39,7 +40,7 @@ def is_packet_of_interest(packet: Packet) -> bool:
         elif not has_transport_filter and has_application_filter:
             if packet.application_layer is None:
                 return False
-            if packet.network_layer.network_type.name in args.network_include and packet.application_layer.application_type in args.application_include:
+            if packet.network_layer.network_type.name in args.network_include and packet.application_layer.application_type.name in args.application_include:
                 return True
 
     else:
@@ -56,7 +57,7 @@ def is_packet_of_interest(packet: Packet) -> bool:
         elif not has_transport_filter and has_application_filter:
             if packet.application_layer is None:
                 return False
-            if packet.application_layer.application_type in args.application_include:
+            if packet.application_layer.application_type.name in args.application_include:
                 return True
 
     return False
@@ -74,6 +75,9 @@ def main():
         if is_packet_of_interest(packet):
             packet.print_packet()
             print("--------------------------------------------------------------------------------------------")
+            if args.save:
+                pcapng_saver.save_packet(packet)
+
         del packet
         gc.collect()
     return
@@ -82,16 +86,17 @@ def main():
 if __name__ == '__main__':
     packets_set = set()
 
-    allowed_link_type_name = list(i.name for i in LinkType)
     allowed_network_type_name = list(i.name for i in NetworkType)
     allowed_transport_type_name = list(i.name for i in TransportType)
     allowed_application_type_name = list(i.name for i in ApplicationType)
 
     arg_parser = argparse.ArgumentParser(description="A packet sniffer in the works.",
-                                         epilog="Developed by Gerald Lim Wee Koon",
+                                         epilog="Developed by Gerald Lim Wee Koon (github: mathscantor)",
                                          formatter_class=argparse.RawTextHelpFormatter)
 
     group = arg_parser.add_mutually_exclusive_group()
+    arg_parser.add_argument('--save', dest='save', required=False, action='store_true',
+                            help="Specify this argument to save sniffed packets into a pcapng file.")
     arg_parser.add_argument('--network', dest='network_include', metavar="", nargs='+', type=str, required=False,
                             choices=allowed_network_type_name, help="Supported Formats: {}".format(allowed_network_type_name))
     group.add_argument('--transport', dest='transport_include', metavar="", nargs='+', type=str, required=False,
@@ -99,6 +104,7 @@ if __name__ == '__main__':
     group.add_argument('--application', dest='application_include', metavar="", nargs='+', type=str, required=False,
                        choices=allowed_application_type_name, help="Supported Formats: {}".format(allowed_application_type_name))
 
+    arg_parser.set_defaults(save=False)
     args = arg_parser.parse_args()
 
     has_network_filter = False
@@ -111,6 +117,7 @@ if __name__ == '__main__':
         has_transport_filter = True
     if args.application_include is not None:
         has_application_filter = True
+
+    pcapng_saver = PcapngSaver()
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ utils/pcapng_saver.py
@@ -0,0 +1,29 @@
+import os
+from datetime import datetime
+from utils.pcapng.writer import FileWriter
+import utils.pcapng.blocks as blocks
+from utils.packet import Packet
+
+
+# Global Options
+SHB_INTERFACE_OPTIONS = {"if_description": "Hand-rolled", "if_os": "Python"}
+SHB_HEADER_OPTIONS = {"shb_hardware": "artificial", "shb_os": "python", "shb_userappl": "python-pcapng"}
+
+
+class PcapngSaver:
+
+    def __init__(self):
+        self.__file_path = "capture/capture_{}.pcapng".format(datetime.now().strftime('%d-%m-%Y_%H:%M:%S'))
+        os.makedirs(os.path.dirname(self.__file_path), exist_ok=True)
+
+        self.__shb = blocks.SectionHeader(options=SHB_HEADER_OPTIONS)
+        self.__shb.new_member(blocks.InterfaceDescription, link_type=1, options=SHB_INTERFACE_OPTIONS)
+
+    def save_packet(self, packet: Packet):
+        with open(self.__file_path, 'ab') as file_obj:
+            writer = FileWriter(file_obj, self.__shb)
+            spb = self.__shb.new_member(blocks.SimplePacket)
+            spb.packet_data = packet.raw_data
+            writer.write_block(spb)
+
+
